10b211976696abbee607bb0db414c219209afb99
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-09-12 18:14:11 -0300

    feat: ensure Login starts with input email and password required

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Styles from './input-styles.scss'
+import Context from '@/presentation/contexts/form/form-context'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
-const Input: React.FC<Props> = (props: Props) => (
-  <div className={Styles.inputWrap}>
-    <input {...props} />
-    <span className={Styles.status}>*</span>
-  </div>
-)
+const Input: React.FC<Props> = (props: Props) => {
+  const value = useContext(Context)
+  const message = value[`${props.name}Error`]
+  const getStatus = (): string => '*'
+  const getTitle = (): string => message
+  return (
+    <div className={Styles.inputWrap}>
+      <input {...props} />
+      <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
+    </div>
+  )
+}
 
 export default Input
