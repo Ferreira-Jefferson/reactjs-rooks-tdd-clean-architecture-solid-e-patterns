74666bef2fad97c0667795e93b6ae94058efe865
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-09-12 18:16:37 -0300

    fix: adjust constant name

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -4,10 +4,10 @@ import { Spinner } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
 
 const FormStatus: React.FC = () => {
-  const { isLoadind, errorMessage } = useContext(Context)
+  const { isLoading, errorMessage } = useContext(Context)
   return (
     <div data-testid="error-wrap" className={Styles.errorWrap}>
-      { isLoadind && <Spinner className={Styles.spinner} />}
+      { isLoading && <Spinner className={Styles.spinner} />}
       { errorMessage && <span className={Styles.error}>{errorMessage}</span>}
     </div>
   )
