22db5d0e6a6406f883bb2360d7ec9678bc9e9e9d
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-09-13 14:25:11 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,31 +1,41 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Login from './login'
+
+type SutTypes = {
+  sut: RenderResult
+}
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />)
+  return {
+    sut
+  }
+}
 
 describe('Login Component', () => {
   describe('Initial State', () => {
     it('should not render spinner and error on start', () => {
-      const { getByTestId } = render(<Login />)
-      const errorWrap = getByTestId('error-wrap')
+      const { sut } = makeSut()
+      const errorWrap = sut.getByTestId('error-wrap')
       expect(errorWrap.childElementCount).toBe(0)
     })
 
     it('should submit button disabled', () => {
-      const { getByTestId } = render(<Login />)
-      const submitButton = getByTestId('submit') as HTMLButtonElement
+      const { sut } = makeSut()
+      const submitButton = sut.getByTestId('submit') as HTMLButtonElement
       expect(submitButton.disabled).toBe(true)
     })
 
     it('should input email is required', () => {
-      const { getByTestId } = render(<Login />)
-      const emailStatus = getByTestId('email-status')
+      const { sut } = makeSut()
+      const emailStatus = sut.getByTestId('email-status')
       expect(emailStatus.title).toBe('Campo obrigatório')
       expect(emailStatus.textContent).toBe('*')
     })
 
     it('should input password is required', () => {
-      const { getByTestId } = render(<Login />)
-      const passwordStatus = getByTestId('password-status')
+      const { sut } = makeSut()
+      const passwordStatus = sut.getByTestId('password-status')
       expect(passwordStatus.title).toBe('Campo obrigatório')
       expect(passwordStatus.textContent).toBe('*')
     })
