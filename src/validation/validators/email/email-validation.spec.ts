52b97aad89e690716ce900fceecf80e2e502d7be
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-09-16 12:02:03 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -33,4 +33,10 @@ describe('EmailValidation', () => {
     const error = sut.validate('')
     expect(error).toEqual(new InvalidFieldError('email'))
   })
+
+  it('should return falsy if email is valid', () => {
+    const { sut } = makeSut()
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
+  })
 })
