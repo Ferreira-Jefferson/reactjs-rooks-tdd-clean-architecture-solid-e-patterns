dc6b4bd4cc93041a49193c1f5730f6f7d0bbf5d9
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-09-16 15:20:23 -0300

    perf: make regex a private property of the class to initialize only once in the constructor

--- src/infra/validation/email-regex-validator.ts
+++ src/infra/validation/email-regex-validator.ts
@@ -1,8 +1,13 @@
 import { EmailValidator } from '@/validation/protocols/email-validator'
 
 export class EmailRegexValidator implements EmailValidator {
+  private readonly regex: RegExp
+
+  constructor () {
+    this.regex = /^(([^<>()[\]\\.,;:\s@\\"]+(\.[^<>()\\[\]\\.,;:\s@\\"]+)*)|(\\".+\\"))@(([^<>()[\]\\.,;:\s@\\"]+\.)+[^<>()[\]\\.,;:\s@\\"]{2,})$/i
+  }
+
   validate (email: string): boolean {
-    const regex = /^(([^<>()[\]\\.,;:\s@\\"]+(\.[^<>()\\[\]\\.,;:\s@\\"]+)*)|(\\".+\\"))@(([^<>()[\]\\.,;:\s@\\"]+\.)+[^<>()[\]\\.,;:\s@\\"]{2,})$/i
-    return regex.test(email)
+    return this.regex.test(email)
   }
 }
