35a2513dd93190558597ffbda00bf5f5e2a232b4
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-09-16 16:46:14 -0300

    test: ensure PasswordValidation calls PasswordValidator with correct value

--- src/validation/validators/password/password-validation.spec.ts
+++ src/validation/validators/password/password-validation.spec.ts
@@ -1,11 +1,28 @@
-
-import { InvalidFieldError } from '@/validation/errors'
 import { PasswordValidation } from './password-validation'
+import { PasswordValidator } from '@/validation/protocols'
+import { stubPasswordRegexValidator } from '@/validation/test/stub-password'
+import faker from 'faker'
+
+type SutTypes = {
+  sut: PasswordValidation
+  passwordRegexValidatorStub: PasswordValidator
+}
+
+const makeSut = (): SutTypes => {
+  const passwordRegexValidatorStub = stubPasswordRegexValidator()
+  const sut = new PasswordValidation(passwordRegexValidatorStub, 'password')
+  return {
+    sut,
+    passwordRegexValidatorStub
+  }
+}
 
 describe('PasswordValidation', () => {
-  it('should return error if value is invalid', () => {
-    const sut = new PasswordValidation('password_field', 5)
-    const error = sut.validate('123')
-    expect(error).toEqual(new InvalidFieldError('password_field'))
+  it('should call PasswordValidator with correct value', () => {
+    const { sut, passwordRegexValidatorStub } = makeSut()
+    const validateSpy = jest.spyOn(passwordRegexValidatorStub, 'validate')
+    const password = faker.internet.password()
+    sut.validate(password)
+    expect(validateSpy).toBeCalledWith(password)
   })
 })
