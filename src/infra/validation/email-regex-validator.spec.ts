f27419d25b0a70d77157c5e2be5fb8d2ce18b33c
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-09-16 14:37:01 -0300

    test: ensure EmailRegexValidator return false if email is invalid

---
+++ src/infra/validation/email-regex-validator.spec.ts
@@ -0,0 +1,11 @@
+import { EmailRegexValidator } from './email-regex-validator'
+import faker from 'faker'
+const makeSut = (): EmailRegexValidator => new EmailRegexValidator()
+
+describe('EmailRegexValidator', () => {
+  it('should return false if email is invalid', () => {
+    const sut = makeSut()
+    const isValid = sut.validate(faker.random.word())
+    expect(isValid).toBe(false)
+  })
+})
