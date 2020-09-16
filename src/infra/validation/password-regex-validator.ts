2f7c8dcdd2a4c460edc9e4c949828164fbe57b51
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-09-16 20:36:24 -0300

    feat: make sure that the maximum size is obeyed if rules.max is passed

--- src/infra/validation/password-regex-validator.ts
+++ src/infra/validation/password-regex-validator.ts
@@ -7,11 +7,23 @@ export class PasswordRegexValidator implements PasswordValidator {
       return true
     }
     this.password = password
-    return this.min(rules.min)
+    const results = []
+    if (rules.min) {
+      results.push(this.min(rules.min))
+    }
+    if (rules.max) {
+      results.push(this.max(rules.max))
+    }
+    return !results.includes(false)
   }
 
   private min (min: number): boolean {
     const regex = new RegExp(`^.{${min},}$`, 'gi')
+    return regex.test(this.password)
+  }
+
+  private max (max: number): boolean {
+    const regex = new RegExp(`^.{0,${max}}$`, 'gi')
     return regex.test(this.password)
   }
 }
