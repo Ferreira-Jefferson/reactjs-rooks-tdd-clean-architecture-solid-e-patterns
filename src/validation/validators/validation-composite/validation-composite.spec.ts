a071b5d28a4d0acf97f891eb8ab94f064e974f71
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-09-17 14:13:53 -0300

    test: ensure ValidationComposite returns the first error if more than one validation fails

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -11,4 +11,15 @@ describe('ValidationComposite', () => {
     const error = sut.validate('any_field', 'any_value')
     expect(error).toBe(errorField.message)
   })
+
+  it('should return the first error if more than one validation fails', () => {
+    const fieldValidationSpy1 = new FieldValidationSpy('any_field')
+    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
+    const sut = new ValidationComposite([fieldValidationSpy1, fieldValidationSpy2])
+    const firstError = new Error('first_error_message')
+    fieldValidationSpy1.error = firstError
+    fieldValidationSpy2.error = new Error('second_error_message')
+    const error = sut.validate('any_field', 'any_value')
+    expect(error).toBe(firstError.message)
+  })
 })
