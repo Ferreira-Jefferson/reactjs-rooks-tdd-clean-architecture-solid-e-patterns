f4b503f2348eb2ddbf1ec629d6fb2c4a892b1bb2
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-09-19 14:01:53 -0300

    feat: ensure Login calls SaveAccessToken with correct value

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Login } from '@/presentation/pages'
-import { makeRemoteAuthentication, makeLoginValidation } from '@/main/factories'
+import { makeRemoteAuthentication, makeLoginValidation, makeSaveAccessToken } from '@/main/factories'
 
 export const makeLogin: React.FC = () => {
   return (
     <Login
       authentication={makeRemoteAuthentication()}
       validation={makeLoginValidation()}
+      saveAccessToken={makeSaveAccessToken()}
     />
   )
 }
