be94713520c687506ce53fbec514c113a82be15c
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-09-20 21:38:40 -0300

    test: ensure SignUp input name is required

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -18,6 +18,26 @@ const testButtonIsDisabled = (sut: RenderResult, fieldName: string, isDisabled: 
   expect(element.disabled).toBe(isDisabled)
 }
 
+const testStatusField = (sut: RenderResult, fieldName: string, validationError?: string): void => {
+  if (validationError) {
+    testValidateFails(sut, fieldName, validationError)
+  } else {
+    testValidateSuccess(sut, fieldName)
+  }
+}
+
+const testValidateFails = (sut: RenderResult, testId: string, errorMessage: string): void => {
+  const passwordStatus = sut.getByTestId(testId)
+  expect(passwordStatus.title).toBe(errorMessage)
+  expect(passwordStatus.textContent).toBe('✗')
+}
+
+const testValidateSuccess = (sut: RenderResult, testId: string): void => {
+  const emailStatus = sut.getByTestId(testId)
+  expect(emailStatus.title).toBe('Campo preenchido corretamente')
+  expect(emailStatus.textContent).toBe('✓')
+}
+
 describe('Signup Component', () => {
   describe('Initial State', () => {
     it('should not render spinner and error on start', () => {
@@ -29,5 +49,11 @@ describe('Signup Component', () => {
   it('should submit button disabled', () => {
     const sut = makeSut()
     testButtonIsDisabled(sut, 'submit', true)
+  })
+
+  it('should input name is required', () => {
+    const sut = makeSut()
+    const errorMessage = 'Campo obrigatório'
+    testStatusField(sut, 'name-status', errorMessage)
   })
 })
