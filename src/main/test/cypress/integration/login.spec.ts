99444f0139032b747365e0d6687e3c068bd7d47d
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-09-24 11:27:31 -0300

    test: ensure login present error state if form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,3 +1,5 @@
+import faker from 'faker'
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
@@ -17,5 +19,19 @@ describe('Login', () => {
     cy.getByTestId('toSignup')
       .should('contain.text', 'Criar conta')
       .should('have.attr', 'href', '/signup')
+  })
+
+  it('should present error state if form is invalid', () => {
+    cy.getByTestId('email').type(faker.random.word())
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo email inválido')
+      .should('contain.text', '✗')
+    cy.getByTestId('password').type(faker.random.word())
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo password inválido')
+      .should('contain.text', '✗')
+    cy.getByTestId('submit')
+      .should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
