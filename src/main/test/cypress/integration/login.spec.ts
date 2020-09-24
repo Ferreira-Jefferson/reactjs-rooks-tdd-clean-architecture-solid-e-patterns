bfb32f79bb7adbc01746f58f8fe91ced5d6aebd0
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-09-24 11:09:10 -0300

    test: ensure login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,21 @@
 describe('Login', () => {
-  it('should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+
+  it('should load with correct initial state', () => {
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '✗')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '✗')
+    cy.getByTestId('submit')
+      .should('contain.text', 'Entrar')
+      .should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+    cy.getByTestId('toSignup')
+      .should('contain.text', 'Criar conta')
+      .should('have.attr', 'href', '/signup')
   })
 })
